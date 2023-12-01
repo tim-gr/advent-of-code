84cbc53e682c8b3908922395cc0dd1a1fc8a8b8f
Tim Grube <37782001+tim-gr@users.noreply.github.com>
2023-12-01 08:12:11 +0100

    Day 01-2023

--- src/main/kotlin/aoc2023/Day01.kt
+++ src/main/kotlin/aoc2023/Day01.kt
@@ -9,16 +9,55 @@ fun main() {
     Day01.task2(input).println()
 
     // Examples
-    Day01.task1(listOf("")).println()
+    Day01.task1(listOf("a1b2c3d4e5f")).println()
 }
 
 object Day01 {
 
     fun task1(input: List<String>): Int {
-        return 0
+        return input.sumOf { string ->
+            ("" + string.first { it.isDigit() } + string.last { it.isDigit() }).toInt()
+        }
     }
 
     fun task2(input: List<String>): Int {
-        return 0
+        return input.sumOf { string ->
+            (findFirstOccurrence(string).toNumber() + findLastOccurrence(string).toNumber()).toInt()
+        }
+    }
+
+    private fun findFirstOccurrence(string: String): String {
+        for (number in DIGITS) {
+            if (string.startsWith(number)) {
+                return number
+            }
+        }
+        return findFirstOccurrence(string.substring(1))
+    }
+
+    private fun findLastOccurrence(string: String): String {
+        for (number in DIGITS) {
+            if (string.endsWith(number)) {
+                return number
+            }
+        }
+        return findLastOccurrence(string.substring(0, string.lastIndex))
+    }
+
+    private val DIGITS = listOf(
+        "1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+    )
+
+    private fun String.toNumber(): String = when(this) {
+        "one" -> "1"
+        "two" -> "2"
+        "three" -> "3"
+        "four" -> "4"
+        "five" -> "5"
+        "six" -> "6"
+        "seven" -> "7"
+        "eight" -> "8"
+        "nine" -> "9"
+        else -> this
     }
 }
--- src/test/kotlin/aoc2023/CheckSolutions.kt
+++ src/test/kotlin/aoc2023/CheckSolutions.kt
@@ -9,12 +9,12 @@ class CheckSolutions {
     @Test
     fun `2023 day 01 task 1`() {
         val input = readInputFile(year = 2023, day = 1)
-        assertEquals(0, Day01.task1(input))
+        assertEquals(55386, Day01.task1(input))
     }
 
     @Test
     fun `2023 day 01 task 2`() {
         val input = readInputFile(year = 2023, day = 1)
-        assertEquals(0, Day01.task2(input))
+        assertEquals(54824, Day01.task2(input))
     }
 }
