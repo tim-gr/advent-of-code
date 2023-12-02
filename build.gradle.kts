017d7b9f16f2a5b4e03912a8faeebe3b6c083e55
Tim Grube <37782001+tim-gr@users.noreply.github.com>
2023-12-02 14:42:45 +0100

    Upgrade gradle and kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,5 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    kotlin("jvm") version "1.7.20"
+    kotlin("jvm") version "1.9.21"
     application
 }
 
@@ -20,8 +18,8 @@ tasks.test {
     useJUnitPlatform()
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+kotlin {
+    jvmToolchain(19)
 }
 
 application {
