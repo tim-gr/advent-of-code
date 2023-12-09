4392a7e9e533afd05cfb5b600b44199f95ca3587
Tim Grube <37782001+tim-gr@users.noreply.github.com>
2023-12-09 12:11:41 +0100

    Formatting

--- src/main/kotlin/aoc2023/Day09.kt
+++ src/main/kotlin/aoc2023/Day09.kt
@@ -17,14 +17,12 @@ fun main() {
 object Day09 {
 
     fun task1(input: List<String>) = input.sumOf { line ->
-            getHistoryRows(line).sumOf { it.last() }
-        }
+        getHistoryRows(line).sumOf { it.last() }
+    }
 
     fun task2(input: List<String>) = input.sumOf { line ->
-            getHistoryRows(line).dropLast(1).foldRight(0L) { numbers, acc ->
-                numbers[0] - acc
-            }
-        }
+        getHistoryRows(line).dropLast(1).foldRight(0L) { numbers, acc -> numbers[0] - acc }
+    }
 
     private fun getHistoryRows(line: String): List<List<Long>> {
         val historyRows = mutableListOf(line.split(" ").map { it.toLong() })
